03214e882923d1d88c44e8398e4295cec2d9f366
Bảo Đặng <baodang.31191023095@st.ueh.edu.vn>
2021-10-15 08:26:49 +0700

    First commit

---
+++ Interface_UI/BUS/Validators/HoSoDaiLyValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_UI.BUS.Validators
+{
+    public class HoSoDaiLyValidator
+    {
+        public string MessageFailure { get; set; }
+
+        public bool KiemTraThongTinDaiLy(int id, string ten, string sdt, int maquan, string email, int maloai, string diachi)
+        {
+            // kiem tra thong tin dai ly
+
+            return true;
+        }
+    }
+}
